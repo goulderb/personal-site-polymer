cc0c669be1ba34f0b85dc605175f44f2a0782bcf
Brett Goulder <goulderba@gmail.com>
2015-06-06 22:38:26 -0400

    Replace app-router with page.js for routing. Switch from individual project pages to a list of projects.

---
+++ routing/page-routes.js
@@ -0,0 +1,16 @@
+(function(document) {
+  var application = document.querySelector("#application");
+
+  page('/', function () {
+    application.route = "home";
+  });
+
+  page('/projects', function (data) {
+    application.route = "projects";
+    application.params = data.params;
+  });
+
+  page.start({
+    hashbang: true
+  });
+});
